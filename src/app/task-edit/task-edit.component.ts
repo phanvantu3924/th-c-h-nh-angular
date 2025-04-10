011f52008a51e61df52a5bce4f37d28a61e1f282
phanvantu3924 <phantu3924@gmail.com>
2025-04-10 22:08:05 +0700

    Initial commit

---
+++ src/app/task-edit/task-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-edit',
+  imports: [],
+  templateUrl: './task-edit.component.html',
+  styleUrl: './task-edit.component.css'
+})
+export class TaskEditComponent {
+
+}
---
+++ src/app/task-new/task-new.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-new',
+  imports: [],
+  templateUrl: './task-new.component.html',
+  styleUrl: './task-new.component.css'
+})
+export class TaskNewComponent {
+
+}
